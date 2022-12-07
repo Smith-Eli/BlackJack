6742d1024a5366d351393d4d2a5b9663b14cea2c
Smith-Eli <235047@tldsb.me>
2022-12-07 13:20:26 -0500

    .

---
+++ blackjack/src/finalTotal.java
@@ -0,0 +1,11 @@
+public class finalTotal {
+    public static void main(String s[]) {
+        //U+1F0C4
+       // String card = Character.toString((char)(0x1f0c4+1));
+
+        //System.out.println("hi" + card);
+        BlackJack bj = new BlackJack();
+        bj.mainBlackJack();
+    
+}
+}
